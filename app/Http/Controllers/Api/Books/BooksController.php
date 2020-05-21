7effdc0f8601a822480aef6daa102610c6d55f21
Phuc Nguyen <phucnguyenbw@gmail.com>
2020-05-21 13:20:32 +0700

    first commit

---
+++ app/Http/Controllers/Api/Books/BooksController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api\Books;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Books\BookRequest;
+use Illuminate\Http\Response;
+use App\Http\Resources\BookResource;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Book;
+
+class BooksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return BookResource::collection(Book::paginate(25));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BookRequest $request)
+    {
+        try {
+            $book = new Book;
+            $book->fill($request->validated())->save();
+
+            return new BookResource($book);
+
+        } catch(\Exception $exception) {
+            throw new HttpException(400, "Invalid data - {$exception->getMessage}");
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::findOrfail($id);
+
+        return new BookResource($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(BookRequest $request, $id)
+    {
+        if (!$id) {
+            throw new HttpException(400, "Invalid id");
+        }
+
+        try {
+           $book = Book::find($id);
+           $book->fill($request->validated())->save();
+
+           return new BookResource($book);
+
+        } catch(\Exception $exception) {
+           throw new HttpException(400, "Invalid data - {$exception->getMessage}");
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $book = Book::findOrfail($id);
+        $book->delete();
+
+        return response()->json(null, 204);
+    }
+}
